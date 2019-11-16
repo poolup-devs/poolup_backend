e3c40e02d4872589fc6883971d497025186d4c1c
Evan Lin <elin4046@gmail.com>
2019-11-16 14:23:52 -0800

    Create review schema

---
+++ src/review/review.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose'); 
+const reviewSchema = new mongoose.Schema({
+    datePosted: {
+        type: Date, 
+        default: Date.now 
+    }, 
+    reviewerUsername: {
+        type: String, 
+        required: true
+    },
+    revieweeUsername: {
+        type: String, 
+        required: true, 
+        index: true 
+    },  
+    rating: {
+        type: Number, 
+        required: true
+    }, 
+    body: String, 
+    rideId: {
+        type: ObjectId, 
+        required: true 
+    }
+}); 
+
+const Review = mongoose.model("Review", reviewSchema); 
+module.exports = {Review}; 

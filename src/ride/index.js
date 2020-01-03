8e32849eae94010980153a71dcec3c2a84b07fe6
Erick Suarez <mr.ericksuarez@gmail.com>
2020-01-03 13:04:33 -0800

    Edit Ride Details to grab ride from query params

--- src/ride/index.js
+++ src/ride/index.js
@@ -138,8 +138,8 @@ router.delete("/rides/delete-ride", checkAuth, (req, res) => {
 });
 
 //Get Ride Details
-router.get("/ride-details", (req, res) => {
-  const rideID = req.body.ride_id;
+router.get("/ride-details", checkAuth, (req, res) => {
+  var rideID = req.query.ride_id;
 
   db.rideDetails(rideID, (err, data) => {
     if (err) {
@@ -149,118 +149,5 @@ router.get("/ride-details", (req, res) => {
     }
   });
 });
-
-// ======================================================
-// STRIPE: TODO: Move over stripe stuff on new folder
-// ======================================================
-
-// Send back Stripe Public Key
-router.get("/public-key", (req, res) => {
-  res.send({ publicKey: process.env.STRIPE_API_KEY });
-});
-
-// Create a Payment Intent
-router.post("/rides/create-payment-intent", checkAuth, (req, res) => {
-  const rideID = req.body.ride_id;
-  const spotsToBePurchased = req.body.spots_to_be_purchased;
-  const authUsername = tokenParser(req.headers.authorization).username;
-  const currency = "usd";
-  let customer;
-  let ride;
-
-  // Get Ride Details
-  db.rideDetails(rideID, (err, data) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      ride = data;
-    }
-  });
-
-  // Get User Information
-  db.getMyInfo(authUsername, (err, user) => {
-    if (err) {
-      console.log("Unable to get user information")
-      res.sendStatus(500);
-    } else {
-       customer = user;
-    }
-  });
-
-  // Set up stripe object
-  const stripe = require('stripe')(process.env.STRIPE_API_KEY);
-
-  // Send back the client secret of the Payment Intent
-  stripe.paymentIntents.create(
-    {
-      amount: ride.price * spotsToBePurchased,
-      currency: currency,
-      payment_method_types: ['card'],
-      customer: customer.stripeID,
-      metadata: {
-        "ride_id": ride._id,
-        "customer_username": authUsername,
-        "checkout_session_id": "test",
-      },
-      receipt_email: customer.email, 
-    },
-    function(err, paymentIntent) {
-      if(err) {
-        res.sendStatus(500);
-      } else {
-        res.sendStatus(200).send(paymentIntent.client_secret);
-      }
-    }
-  );
-});
-
-// Use body-parser to retrieve the raw body as a buffer
-// const bodyParser = require('body-parser');
-
-// Webhook, handles events sent from Stripe
-// app.post('/webhook', bodyParser.raw({type: 'application/json'}), (request, response) => {
-//   let event;
-
-//   try {
-//     event = JSON.parse(request.body);
-//   }
-//   catch (err) {
-//     response.status(400).send(`Webhook Error: ${err.message}`);
-//   }
-
-//   // Handle the event
-//   switch (event.type) {
-//     case 'payment_intent.succeeded':
-//       const paymentIntent = event.data.object;
-//       handlePaymentIntentSucceeded(paymentIntent);
-//       break;
-//     // ... handle other event types
-//     default:
-//       // Unexpected event type
-//       return response.status(400).end();
-//   }
-
-//   // Return a response to acknowledge receipt of the event
-//   response.json({received: true});
-// });
-
-// const handlePaymentIntentSucceeded = (paymentIntent) => {
-    
-//   // Add User to ride
-//   db.joinRide(paymentIntent.metadata["ride"].ownerUsername, paymentIntent.metadata["ride"]._id, paymentIntent.metadata["customer_username"], (err, data) => {
-//     if (err) {
-//       res.sendStatus(500);
-//     } else if (data.length === 0) {
-//       res.status(404).send({
-//         message: "ERROR: The ride is full"
-//       });
-//     } else {
-//       res.status(200).send(data);
-//     }
-//   });
-
-//   // Update Checkout_Session Object to a 'completed' status
-//   // Update Session object using paymentIntent.metadata["checkout_session_id"]
-// }
 
 module.exports = router;

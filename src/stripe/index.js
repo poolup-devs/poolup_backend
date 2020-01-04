1c0367b7a365961dbcf48f1aa1f2785c16659daa
Erick Suarez <mr.ericksuarez@gmail.com>
2020-01-03 21:06:45 -0800

    Add Webhook Endpoint

--- src/stripe/index.js
+++ src/stripe/index.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 
 const checkAuth = require("../middleware/jwt_authenticator.js");
 const tokenParser = require("../utils/token-parser.js");
+const bodyParser = require('body-parser');
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 const rideDB = require("../ride/controller.js");
 const userDB = require("../user/controller.js");
@@ -63,5 +64,70 @@ router.post("/stripe/create-payment-intent", checkAuth, (req, res) => {
     }
   });
 });
+
+  
+// Webhook, handles events sent from Stripe
+router.post('/stripe/webhook', async (req, res) => {
+  let data;
+  let eventType;
+
+  // Check if webhook signing is configured.
+  if (process.env.STRIPE_WEBHOOK_SECRET) {
+    // Retrieve the event by verifying the signature using the raw body and secret.
+    let event;
+    let signature = req.headers["stripe-signature"];
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        req.rawBody,
+        signature,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      console.log(`⚠️ Webhook signature verification failed.`);
+      return res.sendStatus(400);
+    }
+    // Extract the object from the event.
+    data = event.data;
+    eventType = event.type;
+  } else {
+    // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+    // retrieve the event data directly from the request body.
+    data = req.body.data;
+    eventType = req.body.type;
+  }
+
+  if (eventType === "payment_intent.succeeded") {
+    // Fulfill any orders, e-mail receipts, etc
+    handlePaymentIntentSucceeded(data);
+  }
+
+  if (eventType === "payment_intent.payment_failed") {
+    // Notify the customer that their order was not fulfilled
+    console.log("❌ Payment failed.");
+  }
+
+  res.sendStatus(200);
+});
+  
+const handlePaymentIntentSucceeded = (paymentIntent) => {
+  console.log("💰 Payment received!");
+  
+  // Add User to ride
+  db.joinRide(paymentIntent.metadata["ride"].ownerUsername, paymentIntent.metadata["ride"]._id, paymentIntent.metadata["customer_username"], (err, data) => {
+    if (err) {
+      res.sendStatus(500);
+    } else if (data.length === 0) {
+      res.status(404).send({
+        message: "ERROR: The ride is full"
+      });
+    } else {
+      res.status(200).send(data);
+    }
+  });
+  
+  // TODO: Update Checkout_Session Object to a 'completed' status
+  // TODO: Update Session object using paymentIntent.metadata["checkout_session_id"]
+}
   
 module.exports = router;

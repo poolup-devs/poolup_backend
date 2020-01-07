daac6af7cf624b6ba3bbf215f184ba208921c5c7
Erick Suarez <mr.ericksuarez@gmail.com>
2020-01-07 13:54:27 -0800

    rm auth for getting public key for dev purposesand send json for paymentintent

--- src/stripe/index.js
+++ src/stripe/index.js
@@ -9,15 +9,15 @@ const rideDB = require("../ride/controller.js");
 const userDB = require("../user/controller.js");
 
 // Send back Stripe Public Key
-router.get("/stripe/public-key", checkAuth, (req, res) => {
-    res.sendStatus(200).send({ publicKey: process.env.STRIPE_PUBLIC_KEY });
+router.get("/stripe/public-key", (req, res) => {
+    res.status(200).send({ publicKey: process.env.STRIPE_PUBLIC_KEY });
 });
   
 // Create a Payment Intent
-router.post("/stripe/create-payment-intent", checkAuth, (req, res) => {
+router.post("/stripe/create-payment-intent", (req, res) => {
   const rideID = req.body.ride_id;
   const spotsToBePurchased = req.body.spots_to_be_purchased;
-  const authUsername = tokenParser(req.headers.authorization).username;
+  const authUsername = req.query.username;
   const currency = "usd";
 
   // Get Ride Details
@@ -41,7 +41,7 @@ router.post("/stripe/create-payment-intent", checkAuth, (req, res) => {
                 amount: ride.price * spotsToBePurchased * 100,
                 currency: currency,
                 payment_method_types: ['card'],
-                customer: customer.stripeID,
+                //customer: customer.stripeID,
                 metadata: {
                     "ride_id": rideID,
                     "customer_username": authUsername,
@@ -54,7 +54,7 @@ router.post("/stripe/create-payment-intent", checkAuth, (req, res) => {
                     res.sendStatus(500);
                     return;
                 } else {
-                    res.status(200).send(paymentIntent.client_secret);
+                    res.status(200).json({ client_secret: paymentIntent.client_secret});
                     return;
                 }
             }

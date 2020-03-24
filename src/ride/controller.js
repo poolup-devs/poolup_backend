f21852036b6068b77c96b620657a53456f3fddba
Evan Lin <elin0467@g.ucla.edu>
2020-03-24 15:40:49 -0700

    Fix edge case where the ride may no longer exist when the completed rides task runs

--- src/ride/controller.js
+++ src/ride/controller.js
@@ -155,15 +155,17 @@ const postRide = (rideInfo, callback) => {
 const updateCompletedRidesTask = (rideId) => {
   return async function(){
     const completedRide = await Ride.findById(rideId)
-    // At least a single passenger on the completed ride  
-    if (completedRide.passengers.length > 0) {
-      // Update the driver's number of completed rides based on the number of passengers dropped off 
-      await User.findOneAndUpdate({username: completedRide.ownerUsername},  {$inc: {ridesCompleted: completedRide.passengers.length}})
+    if (completedRide) {
+      // At least a single passenger on the completed ride  
+      if (completedRide.passengers.length > 0) {
+        // Update the driver's number of completed rides based on the number of passengers dropped off 
+        await User.findOneAndUpdate({username: completedRide.ownerUsername},  {$inc: {ridesCompleted: completedRide.passengers.length}})
 
-      // Update each passenger's number of completed rides by 1 
-      completedRide.passengers.forEach(async (passenger) => {
-        await User.findOneAndUpdate({username: passenger}, {$inc: {ridesCompleted: 1}})
-      })
+        // Update each passenger's number of completed rides by 1 
+        completedRide.passengers.forEach(async (passenger) => {
+          await User.findOneAndUpdate({username: passenger}, {$inc: {ridesCompleted: 1}})
+        })
+      }
     }
   }
 }

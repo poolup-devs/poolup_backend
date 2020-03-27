83f3982c12450af1cb5888c8db0ff26eb2575b51
Evan Lin <elin0467@g.ucla.edu>
2020-03-27 13:55:04 -0700

    Add scheduler task and refactor the location for scheduled tasks

---
+++ src/tasks/scheduler.js
@@ -0,0 +1,27 @@
+var schedule = require('node-schedule');
+
+// Schedule a task to run once, X hours after a certain date
+const scheduleTaskHoursAfterDate = (uniqueTaskName, task, date, hours) => {
+    var scheduledDate = new Date(date)
+    scheduledDate.setHours(scheduledDate.getHours() + hours)      
+    schedule.scheduleJob(uniqueTaskName, scheduledDate, task) 
+}
+
+const cancelTask = (taskName) => {
+    const task = schedule.scheduledJobs[taskName]
+    task.cancel()
+}
+
+// Clean up all tasks associated with a ride, such as when a ride is cancelled and deleted 
+const cancelTasksAssociatedWithRide = (rideId) => {
+    const tasksToCancel = ['updateCompletedRidesTask.${rideId}']
+    tasksToCancel.forEach((task) => {
+        cancelTask(task)
+    })
+}
+
+module.exports = {
+    scheduleTaskHoursAfterDate,
+    cancelTask, 
+    cancelTasksAssociatedWithRide
+}

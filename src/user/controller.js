9f09e2cd376c81c2896226515742d58fc8c19546
Evan Lin <elin0467@g.ucla.edu>
2020-04-16 22:25:53 -0700

    Send emails using nodemailer instead of SendGrid, and render them dynamically locally.

--- src/user/controller.js
+++ src/user/controller.js
@@ -1,11 +1,8 @@
 const User = require("./user").User;
 const Ride = require("../ride/ride.js").Ride;
 const Noti = require("../noti/noti.js").Noti;
-const Review = require("../review/review").Review;
 const jwt = require("jsonwebtoken");
-const sgMail = require("@sendgrid/mail");
-
-
+const Email = require("../utils/email/email")
 
 
 // Users require a certain minimum amount of ratings to calculate an average rating
@@ -91,42 +88,13 @@ const sendVerificationEmail = (email) => {
         // Construct verification email
         const token = jwt.sign({ email }, process.env.JWT_EMAIL_KEY, { expiresIn: 60*30 });
         if (process.env.MODE === "STAGING") {
-          var url =
-            "localhost:" + process.env.PORT + `/users/verify?email=${email}&token=${token}`;
-          var verificationEmail = {
-            to: email,
-            from: "pool-up@outlook.com",
-            subject: "PoolUp: Email Verification Required",
-            text: "Here's the link",
-            html: "<br>Link for local dev: <br>" + url
-          };
+          var verificationUrl = `http://localhost:${process.env.PORT}/users/verify?email=${email}&token=${token}`;
         }
         else {
-          var url =
-            "restapi." +
-            process.env.PRODUCTION_DOMAIN_URL +
-            "/users/verify?token=" +
-            token;
-          var verificationEmail = {
-            to: email,
-            from: "pool-up@outlook.com",
-            templateId: "d-0d8dff79ca8e4d0e8b4b9b1b12038a62",
-            dynamic_template_data: {
-              subject: "PoolUp Email Verification",
-              name: req.body.username,
-              url: url
-            }
-          };
+          var verificationUrl = `http://restapi.${process.env.PRODUCTION_DOMAIN_URL}/users/verify?email=${email}&token=${token}`;
         }
-
-        // Send verification email
-        sgMail.send(verificationEmail)
-          .then(() => {
-            resolve(true)
-          })
-          .catch(error => {
-            reject("Could not send verification email!")
-          });
+        await Email.sendVerificationEmail(email, verificationUrl)
+        resolve(true)
       }
     }
     catch(e) {

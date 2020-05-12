23a667185e2c360ae3ffc99f6773818f86edd460
Erick Suarez <mr.ericksuarez@gmail.com>
2020-05-11 23:20:02 -0700

    Add After All to jest

---
+++ tests/setup.js
@@ -0,0 +1,16 @@
+afterAll(async () => {
+    try {
+      const { todos } = mongoose.connection.collections;
+      // Collection is being dropped.
+      await todos.drop()
+      // Connection to Mongo killed.
+      await mongoose.disconnect();
+      // Server connection closed.
+      await server.close();
+    } catch (error) {
+      console.log(`
+        Error:
+        ${error}
+      `);
+      throw error;
+    }

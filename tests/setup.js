3b8a1de83ef708715e21ad9638e7826301cd8a7d
Erick Suarez <mr.ericksuarez@gmail.com>
2020-05-12 20:57:15 -0700

    Fix Hanging Jest Bug with forceExit

--- tests/setup.js
+++ tests/setup.js
@@ -1,17 +1,1 @@
-afterAll(async () => {
-  try {
-    const { todos } = mongoose.connection.collections;
-    // Collection is being dropped.
-    await todos.drop();
-    // Connection to Mongo killed.
-    await mongoose.disconnect();
-    // Server connection closed.
-    await server.close();
-  } catch (error) {
-    console.log(`
-        Error:
-        ${error}
-      `);
-    throw error;
-  }
-});
+jest.setTimeout(10000); // in milliseconds
